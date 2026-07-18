668c8204c454a07df6207201c31abe0dd4ad71e2
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2026-07-18 21:46:50 +0300

    remove console logs & but all html element up page

--- main.js
+++ main.js
@@ -1,39 +1,34 @@
 const bill = document.querySelector("#bill");
 const tipsBtn = document.querySelectorAll("button.btn");
-let billValue = 0;
+const numOfPeople = document.querySelector("#numOfPeople");
+const tipsForOnePerson = document.querySelector(".tipAmount");
+const personTotal = document.querySelector(".personTotal");
 
+let billValue = 0;
+let tipAmount = 0;
+let totalAmount = 0;
+let numPeople = 0;
 function handleGetBillValue() {
   bill.addEventListener("keyup", () => {
     billValue = Number(bill.value) || 0;
-
-    console.log(billValue);
   });
   return billValue;
 }
 handleGetBillValue();
 
-let tipAmount = 0;
-let totalAmount = 0;
 const calculateTips = () => {
   tipsBtn.forEach((e) => {
     e.addEventListener("click", () => {
       const tipPercentage = Number(e.value);
       tipAmount = billValue * (tipPercentage / 100);
       totalAmount = billValue + tipAmount;
-
-      console.log("قيمة البقشيش:", tipAmount);
-      console.log("المجموع الكلي:", totalAmount);
     });
     return (tipAmount, totalAmount);
   });
 };
 
 calculateTips();
-const numOfPeople = document.querySelector("#numOfPeople");
-const tipsForOnePerson = document.querySelector(".tipAmount");
-const personTotal = document.querySelector(".personTotal");
 
-let numPeople = 0;
 const numberOfPeople = () => {
   numOfPeople.addEventListener("keyup", () => {
     numPeople = Number(numOfPeople.value) || 0;
@@ -42,8 +37,6 @@ const numberOfPeople = () => {
     tipsForOnePerson.innerHTML = `$${tipsForOne} `;
     const totalBill = Math.ceil(totalAmount / numPeople);
     personTotal.innerHTML = `$${totalBill}`;
-    console.log(tipsForOne);
-    console.log(totalBill);
   });
 };
 numberOfPeople();
